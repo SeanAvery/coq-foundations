a0cf31e4a504796e12667987d568a6eba8cb4b99
Sean Pollock <seanpollock3344@gmail.com>
2019-10-18 22:36:41 -0400

    exercise 1

--- logical-foundations/1-functional-programming/Boolean.v
+++ logical-foundations/1-functional-programming/Boolean.v
@@ -53,3 +53,25 @@ Notation "x || y" := (orb x y).
 
 Example test_orb2: false || false || true = true.
 Proof. simpl. reflexivity. Qed.
+
+(*
+  Exercise 1
+  on should return true if either or both of its inputs are false.
+*)
+
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  negb(b1 && b2).
+
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
